ba5888d4a26c39807a3e4a86492ccc27b51aae45
hxqv0 <weiterins@gmail.com>
2021-11-12 20:59:08 +0200

    Fixed color

--- client/components/layout.js
+++ client/components/layout.js
@@ -8,5 +8,6 @@ export default function Layout({ children }) {
       </header>
       <main>{children}</main>
     </>
-  )
+  );
 }
+
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,10 +1,10 @@
-import Link from "next/link"
-import { useState } from "react"
+import Link from "next/link";
+import { useState } from "react";
 
 const LinkItem = ({ href, children, ...props }) => {
   return (
     <Link href={href}>
-      <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded dark:text-white font-bold items-center justify-center hover:bg-pink-600 dark:hover:text-white">
+      <a className="lg:inline-flex color-text lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center hover:bg-pink-600 hover:text-white">
         {children}
       </a>
     </Link>
@@ -12,53 +12,57 @@ const LinkItem = ({ href, children, ...props }) => {
 };
 
 const Navbar = (props) => {
-  const [active, setActive] = useState(false)
+  const [active, setActive] = useState(false);
 
   const handleClick = () => {
-    setActive(!active)
-  }
+    setActive(!active);
+  };
 
-  return (<>
-    <nav className="flex items-center flex-wrap section p-3">
-      <div className="hidden w-full lg:flex-grow lg:w-auto">
-        <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto">
-          <LinkItem href="/">
-            Головна
-          </LinkItem>
-          <LinkItem href="/services">
-            Сервіси
-          </LinkItem>
-          <LinkItem href="/doctors">
-            Лікарі
-          </LinkItem>
-          <LinkItem href="/contact">
-            Записатися
-          </LinkItem>
+  return (
+    <>
+      <nav className="flex items-center flex-wrap section p-3">
+        <div className="hidden w-full lg:flex-grow lg:w-auto">
+          <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto">
+            <LinkItem href="/">Головна</LinkItem>
+            <LinkItem href="/services">Сервіси</LinkItem>
+            <LinkItem href="/doctors">Лікарі</LinkItem>
+            <LinkItem href="/contact">Записатися</LinkItem>
+          </div>
         </div>
-      </div>
-      <button className="inline-flex p-3 hover:bg-pink-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none" onClick={handleClick}>
-        <svg className='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-          <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4 6h16M4 12h16M4 18h16' />
-        </svg>
-      </button>
-      <div className={`${active ? '' : 'hidden'} w-full lg:inline-flex lg:flex-grow lg:w-auto`}>
-        <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto">
-          <LinkItem href="/">
-            Головна
-          </LinkItem>
-          <LinkItem href="/services">
-            Сервіси
-          </LinkItem>
-          <LinkItem href="/doctors">
-            Лікарі
-          </LinkItem>
-          <LinkItem href="/contact">
-            Записатися
-          </LinkItem>
+        <button
+          className="inline-flex p-3 hover:bg-pink-600 rounded lg:hidden color-text ml-auto hover:text-white outline-none"
+          onClick={handleClick}
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+        <div
+          className={`${
+            active ? "" : "hidden"
+          } w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+        >
+          <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto">
+            <LinkItem href="/">Головна</LinkItem>
+            <LinkItem href="/services">Сервіси</LinkItem>
+            <LinkItem href="/doctors">Лікарі</LinkItem>
+            <LinkItem href="/contact">Записатися</LinkItem>
+          </div>
         </div>
-      </div>
-    </nav>
-  </>);
+      </nav>
+    </>
+  );
 };
 
-export default Navbar
+export default Navbar;
