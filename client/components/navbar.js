66f8ac2c4a91769e3a847885319c3dc82cacb19f
hxqv0 <weiterins@gmail.com>
2021-11-15 21:53:47 +0200

    Logo

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,6 +1,7 @@
 import { useTheme } from "next-themes";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import Logo from './logo'
 
 const LinkItem = ({ href, children, ...props }) => {
   return (
@@ -31,38 +32,42 @@ const Navbar = (props) => {
 
   return (
     <>
-      <nav className="flex items-center flex-wrap section p-3">
-        <button
-          className="inline-flex p-3 hover:bg-pink-600 rounded lg:hidden color-text ml-auto hover:text-white outline-none"
-          onClick={handleClick}
-        >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </button>
-        <button className="lg:hidden rounded color-text" onClick={switchTheme}>
-          {theme === "dark" ? (
-            <i className="fas fa-sun pointer-events-none color-text"></i>
-          ) : (
-            <i className="fas fa-moon pointer-events-none color-text"></i>
-          )}
-        </button>
+      <nav className="flex items-center flex-wrap section pl-10 pr-10 p-2 bg-white">
+          <Logo src={
+            theme === "light"
+              ? "/footprint.png"
+              : "/footprint-dark.png"
+          } />
 
+          <button
+            className="inline-flex p-3 hover:bg-pink-600 rounded lg:hidden color-text ml-auto hover:text-white outline-none"
+            onClick={handleClick}
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+          <button className="lg:hidden rounded color-text" onClick={switchTheme}>
+            {theme === "dark" ? (
+              <i className="fas fa-sun pointer-events-none color-text"></i>
+            ) : (
+              <i className="fas fa-moon pointer-events-none color-text"></i>
+            )}
+          </button>
         <div
-          className={`${
-            active ? "" : "hidden"
-          } w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+          className={`${active ? "" : "hidden"
+            } w-full lg:inline-flex lg:flex-grow lg:w-auto`}
         >
           <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto">
             <LinkItem href="/">Головна</LinkItem>
@@ -70,9 +75,8 @@ const Navbar = (props) => {
             <LinkItem href="/doctors">Лікарі</LinkItem>
             <LinkItem href="/contact">Записатися</LinkItem>
             <button
-              className={`${
-                active ? "hidden" : ""
-              } rounded color-text justify-center"`}
+              className={`${active ? "hidden" : ""
+                } rounded color-text justify-center"`}
               onClick={switchTheme}
             >
               {theme === "dark" ? (
