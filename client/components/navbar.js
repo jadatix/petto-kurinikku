c7dc4b22049d01928b623cb97410e7ea11aea17b
hxqv0 <weiterins@gmail.com>
2021-11-15 18:06:58 +0200

    make it better

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -32,24 +32,6 @@ const Navbar = (props) => {
   return (
     <>
       <nav className="flex items-center flex-wrap section p-3">
-        <div className="hidden w-full lg:flex-grow lg:w-auto">
-          <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto">
-            <LinkItem href="/">Головна</LinkItem>
-            <LinkItem href="/services">Сервіси</LinkItem>
-            <LinkItem href="/doctors">Лікарі</LinkItem>
-            <LinkItem href="/contact">Записатися</LinkItem>
-            <button
-              className="rounded color-text justify-center"
-              onClick={switchTheme}
-            >
-              {theme === "dark" ? (
-                <i className="fas fa-sun pointer-events-none color-text"></i>
-              ) : (
-                <i className="fas fa-moon pointer-events-none color-text"></i>
-              )}
-            </button>
-          </div>
-        </div>
         <button
           className="inline-flex p-3 hover:bg-pink-600 rounded lg:hidden color-text ml-auto hover:text-white outline-none"
           onClick={handleClick}
@@ -87,6 +69,18 @@ const Navbar = (props) => {
             <LinkItem href="/services">Сервіси</LinkItem>
             <LinkItem href="/doctors">Лікарі</LinkItem>
             <LinkItem href="/contact">Записатися</LinkItem>
+            <button
+              className={`${
+                active ? "hidden" : ""
+              } rounded color-text justify-center"`}
+              onClick={switchTheme}
+            >
+              {theme === "dark" ? (
+                <i className="fas fa-sun pointer-events-none color-text"></i>
+              ) : (
+                <i className="fas fa-moon pointer-events-none color-text"></i>
+              )}
+            </button>
           </div>
         </div>
       </nav>
