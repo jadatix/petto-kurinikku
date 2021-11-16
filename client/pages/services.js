dd2aed138c2be4201ce6596813ee6a2d8b8bcb26
jadatix <andrijlasenko@gmail.com>
2021-11-16 19:46:51 +0200

    services + mongodb

--- client/pages/services.js
+++ client/pages/services.js
@@ -4,21 +4,21 @@ const ServiceItem = (props) => {
   return (
     <>
       <div className="p-4 lg:w-1/3">
-        <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+        <div className="dark:bg-gray-800 h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
           <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-            CATEGORY
+            {props.price} &#8372;
           </h2>
-          <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+          <h1 className="dark:text-white title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
             {props.name}
           </h1>
           <p className="leading-relaxed mb-3">{props.description}</p>
-          <a className="text-pink-500 inline-flex items-center">
+          {/* <a className="text-pink-600 inline-flex items-center">
             Записатися
             <svg
               className="w-4 h-4 ml-2"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              stroke-width="2"
+              stroke-width="2"  
               fill="none"
               stroke-linecap="round"
               stroke-linejoin="round"
@@ -26,7 +26,7 @@ const ServiceItem = (props) => {
               <path d="M5 12h14"></path>
               <path d="M12 5l7 7-7 7"></path>
             </svg>
-          </a>
+          </a> */}
         </div>
       </div>
     </>
@@ -60,6 +60,7 @@ const Services = ({ services }) => {
               <ServiceItem
                 name={service.name}
                 description={service.description}
+                price={service.price}
               />
             );
           })}
